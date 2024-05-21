59eed4622d518f25613d6d2ba5be00555df336c7
unknown <sh911124@gmail.com>
2024-05-22 00:21:00 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
+import { useEffect, useState } from "react";
+
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import api from "./utils/api";
 
 import TodoBoard from "./components/TodoBoard";
 
@@ -8,22 +11,64 @@ import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 
 function App() {
+  const [todoList, setTodoList] = useState([]);
+  const [todoValue, setTodoValue] = useState("");
+
+  const getTasks = async () => {
+    const response = await api.get("/tasks");
+    console.log(response);
+    setTodoList(response.data.data);
+  };
+
+  useEffect(() => {
+    getTasks();
+  }, []);
+
+  const addTask = async () => {
+    try {
+      const response = await api.post("/tasks", {
+        task: todoValue,
+        isComplete: false,
+      });
+      if (response.status === 200) {
+        console.log("성공");
+        setTodoValue("");
+        getTasks();
+      } else {
+        throw new Error("task can not be added");
+      }
+    } catch (err) {
+      console.log("error ", err);
+    }
+  };
+
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter" && todoValue) {
+      addTask();
+    }
+  };
+
   return (
-    <Container>
+    <Container className="container">
       <Row className="add-item-row">
         <Col xs={12} sm={10}>
           <input
             type="text"
             placeholder="할일을 입력하세요"
             className="input-box"
+            value={todoValue}
+            onChange={(event) => setTodoValue(event.target.value)}
+            onKeyPress={handleKeyPress}
           />
         </Col>
         <Col xs={12} sm={2}>
-          <button className="button-add">추가</button>
+          <button className="button-add" onClick={addTask}>
+            추가
+          </button>
         </Col>
       </Row>
 
-      <TodoBoard />
+      <TodoBoard todoList={todoList} getTasks={getTasks} />
     </Container>
   );
 }
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,16 +1,51 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
+import api from "../utils/api";
 
-const TodoItem = () => {
+const TodoItem = ({ item, getTasks }) => {
+  const updateTask = async () => {
+    try {
+      const response = await api.put(`/tasks/${item._id}`);
+
+      if (response.status === 200) {
+        console.log("수정 성공");
+        getTasks();
+      } else {
+        throw new Error("task can not be updated");
+      }
+    } catch (err) {
+      console.log("error ", err);
+    }
+  };
+
+  const deleteTask = async () => {
+    try {
+      const response = await api.delete(`/tasks/${item._id}`);
+
+      if (response.status === 200) {
+        console.log("삭제 성공");
+        getTasks();
+      } else {
+        throw new Error("task can not be deleted");
+      }
+    } catch (err) {
+      console.log("error ", err);
+    }
+  };
+
   return (
     <Row>
       <Col xs={12}>
         <div className={`todo-item`}>
-          <div className="todo-content">밥먹기</div>
+          <div className="todo-content">{item.task}</div>
 
           <div>
-            <button className="button-delete">삭제</button>
-            <button className="button-delete">끝남</button>
+            <button className="button-delete" onClick={deleteTask}>
+              삭제
+            </button>
+            <button className="button-delete" onClick={updateTask}>
+              {item.isComplete ? "끝남" : "안끝남"}
+            </button>
           </div>
         </div>
       </Col>
